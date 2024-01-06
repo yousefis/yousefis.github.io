48145b7bb530d72f0d3e5bccb69992a0038613c9
yousefis <30015128+yousefis@users.noreply.github.com>
2024-01-06 16:07:46 +0100

    Add files via upload

---
+++ style.css
@@ -0,0 +1,129 @@
+body {
+  margin: 0;
+  font-family: sans-serif, arial, helvetica;  
+  background-color: #eee;
+}
+.main {
+  max-width: 768px;
+  margin: auto;
+  padding: 15px;
+  background-color: #fff;
+}
+h1 {
+    text-align: center;
+    font-size: 1.6em;
+}
+.flexible {
+    display: flex;
+    justify-content: center;
+    float: none;
+}
+.flexible img {
+    border-radius: 15%;
+    padding: 0;
+    width: 255px;
+    height: auto;
+    max-width: 95%;
+}
+ul li {
+    margin: 4px;
+    margin-left: 0;
+    padding-bottom: 2px;
+}
+
+@media only screen and (min-width: 768px) {
+    .main {
+	padding: 15px 25px 15px 25px;
+    } 
+    .flexible {
+	justify-content: flex-end;
+	float: right;  
+    }
+    .flexible img {
+	padding: 15px;
+    }
+    h1 {
+	text-align: left;
+	font-size: 2em;
+    }
+}
+
+.pageright {
+  width: 5%;
+  float: right
+}
+
+.boldbig {
+  font-size: 150%;
+  font-weight: bold;
+}
+
+.boldbig2 {
+  font-size: 150%;
+  font-weight: bold;
+  margin-bottom: 10px;
+  margin-top: 10px;
+}
+
+#main_last {
+  padding-bottom: 4%;
+}
+
+.poem {
+  white-space: pre;
+}
+
+#footer {
+  max-width:850px;
+  margin: auto;
+  padding: 2% 5% 2% 5%;
+  font-size: 80%;
+  height: 30px;
+}
+.post {
+  padding: 2% 0 2% 0;
+  font-size: 80%;
+  line-height: 150%;
+}
+.zimbol {
+  color: #999;
+  font-size: 80%;
+}
+img {
+  vertical-align: middle;
+/*  margin-right: 5px;
+  margin-bottom: 5px; */
+}
+p {
+  margin-bottom: 5px;
+}
+ul {
+  margin: 0;
+  margin-top: 10px;
+}
+ul.li {
+  margin: 0;
+  padding: 0;
+}
+a {
+  text-decoration:none;
+}
+a:link, a:active, a:visited{
+  color: #11C;
+}
+a:hover {
+  color: #080;
+  padding-bottom: 1px;
+}
+table {
+  margin: 0;
+}
+table td {
+  padding: 0;
+  padding-left: 50px;
+  vertical-align: top;
+}
+
+p {
+  margin-bottom: 0px;
+}
